2a1cf1363ec99e6f05ca92043378fa583b5d27e1
Nina B <ngbuscemi@gmail.com>
2024-01-11 19:56:07 -0500

    create js file for quote generator

--- assets/script.js
+++ assets/script.js
@@ -2,35 +2,8 @@ function translateText() {
   const inputText = document.getElementById('inputText').value;
   const outputTextElement = document.getElementById('outputText');
 
-  // Check if the input text is empty
-  if (inputText.trim() === '') {
-    // Make a request to the quote API
-    fetch('https://type.fit/api/quotes')
-      .then(response => response.json())
-      .then(quoteData => {
-        console.log('Quote API Response:', quoteData);
 
-        // Check if there are quotes in the response
-        if (quoteData.length > 0) {
-          // Select a random quote from the array
-          const randomIndex = Math.floor(Math.random() * quoteData.length);
-          const randomQuote = quoteData[randomIndex];
 
-          // Assuming the quote text is provided in randomQuote.text
-          if (randomQuote.text) {
-            outputTextElement.innerHTML = randomQuote.text; // Use innerHTML to render emojis
-          } else {
-            outputTextElement.textContent = 'Quote retrieval failed. Please try again.';
-          }
-        } else {
-          outputTextElement.textContent = 'No quotes available. Please try again later.';
-        }
-      })
-      .catch(error => {
-        console.error('Error fetching quote:', error);
-        outputTextElement.textContent = 'Quote retrieval failed. Please try again.';
-      });
-  } else {
     // Make a request to the emoji translation API
     fetch(`https://api.funtranslations.com/translate/emoji.json?text=${encodeURIComponent(inputText)}`, {
       headers: {
@@ -51,8 +24,6 @@ function translateText() {
           // Decode HTML entities (if any) before rendering
           const decodedText = new DOMParser().parseFromString(data.contents.translated, 'text/html').body.textContent;
           outputTextElement.innerHTML = decodedText; // Use innerHTML to render emojis
-        } else {
-          outputTextElement.textContent = 'Emoji translation failed. Please try again.';
         }
       })
       .catch(error => {
@@ -60,7 +31,7 @@ function translateText() {
         outputTextElement.textContent = 'Emoji translation failed. Please try again.';
       });
   }
-}
+
 
 // Attach the translateText function to the button click event
 document.getElementById('translateButton').addEventListener('click', translateText);
